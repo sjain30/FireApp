1e1f3a9c957dab6c5ff3591fa6dc0b912e8233a8
SAJAL JAIN <sjain30sept@gmail.com>
2020-01-02 14:14:22 +0530

    Adding multiple objects

--- app/src/main/java/com/sajal/fireapp/MainActivity.java
+++ app/src/main/java/com/sajal/fireapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
@@ -12,6 +13,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText,editText2;
     private Button button;
     private FirebaseDatabase firebaseDatabase;
     @Override
@@ -19,14 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText=findViewById(R.id.editText);
+        editText2=findViewById(R.id.editText2);
         button=findViewById(R.id.button);
         firebaseDatabase= FirebaseDatabase.getInstance();
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DatabaseReference myRef = firebaseDatabase.getReference("Name");
+//                myRef.setValue("Sajal");
+//
+//            }
+//        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseReference myRef = firebaseDatabase.getReference("Name");
-
-                myRef.setValue("Sajal");
+//                DatabaseReference myRef = firebaseDatabase.getReference(editText2.getText().toString());
+                DatabaseReference myRef = firebaseDatabase.getReference("User");
+                DatabaseReference myRefChild= myRef.child(editText2.getText().toString());
+//                myRef.setValue("Name");
+//                DatabaseReference myRefChild = firebaseDatabase.getReference("https://fireapp-ed027.firebaseio.com/User/Name");
+                String value = editText.getText().toString();
+//                myRef.push().setValue(value);
+                myRefChild.setValue(value);
 
             }
         });
