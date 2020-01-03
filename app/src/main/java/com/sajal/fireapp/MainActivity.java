e7f3413d5ad3ba358c28a6e2a26b49eabb2134b5
SAJAL JAIN <sjain30sept@gmail.com>
2020-01-03 22:09:35 +0530

    Reading from Database

--- app/src/main/java/com/sajal/fireapp/MainActivity.java
+++ app/src/main/java/com/sajal/fireapp/MainActivity.java
@@ -1,21 +1,35 @@
 package com.sajal.fireapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText,editText2;
     private Button button;
     private FirebaseDatabase firebaseDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +60,54 @@ public class MainActivity extends AppCompatActivity {
                 String value = editText.getText().toString();
 //                myRef.push().setValue(value);
                 myRefChild.setValue(value);
+
+            }
+        });
+        DatabaseReference reference = firebaseDatabase.getReference("User");
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                Map<String, Object> map= (Map<String,Object>)dataSnapshot.getValue();
+//                String name = (String) map.get("Name");
+//                TextView text= findViewById(R.id.textView);
+//                String surName = (String) map.get("SurName");
+//                text.setText(name+" "+surName);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        final ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                list.add(dataSnapshot.getValue().toString());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
